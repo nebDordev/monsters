f2006336f903f0cc2f76b7af3d7f61a2884a6a56
nebDordev <60310841+nebDordev@users.noreply.github.com>
2020-04-26 14:01:06 +1200

    Updates

---
+++ precache-manifest.c7fc9c342f54cc29f90c7c6c786e29ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f55bfd87dc4c4ecb874e3975aec3df",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c45cff4cac2dd3c5c039",
+    "url": "/monsters/static/css/main.9f375ba7.chunk.css"
+  },
+  {
+    "revision": "652c3969307ff59b9d1c",
+    "url": "/monsters/static/js/2.be4c3002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.be4c3002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45cff4cac2dd3c5c039",
+    "url": "/monsters/static/js/main.4a3966e8.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  }
+]);
